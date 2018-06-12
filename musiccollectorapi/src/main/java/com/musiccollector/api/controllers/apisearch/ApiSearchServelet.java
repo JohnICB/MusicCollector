1b42df00673eacdb68e0d7a1b21723d244ee551e
Unknown <cristianignat97@gmail.com>
2018-06-12 03:45:28 +0300

    Auto stash before merge of "ServerTest" and "origin/ServerTest"

---
+++ musiccollectorapi/src/main/java/com/musiccollector/api/controllers/apisearch/ApiSearchServelet.java
@@ -0,0 +1,41 @@
+package com.musiccollector.api.controllers.apisearch;
+
+import com.musiccollector.api.musicbrainzapi.TrackQueryBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = "/search")
+public class ApiSearchServelet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        String artist =request.getParameter("seacthtype");
+        String track  =request.getParameter("searchcontent");
+
+
+
+        TrackQueryBuilder tqb = new TrackQueryBuilder();
+
+
+        request.getRequestDispatcher("/WEB-INF/views/TEMPLATE.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        if (Math.random() > 0.5f)
+            response.sendRedirect("welcome");
+        else {
+            request.getRequestDispatcher("/WEB-INF/views/TEMPLATE2.jsp").forward(request, response);
+        }
+    }
+
+}
