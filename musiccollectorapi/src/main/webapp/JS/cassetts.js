d7915c11e074eef63384e593ec0b7daa4617734f
Unknown <cristianignat97@gmail.com>
2018-06-18 22:07:47 +0300

    Moved js to search1

--- musiccollectorapi/src/main/webapp/JS/cassetts.js
+++ musiccollectorapi/src/main/webapp/JS/cassetts.js
@@ -1,22 +1,4 @@
 
-        $(function() {
-
-var $contextMenu = $("#contextMenu");
-
-$("body").on("contextmenu", "ul img", function(e) {
-     $contextMenu.css({
-          display: "block",
-          left: e.pageX,
-          top: e.pageY
-     });
-     return false;
-});
-
-$('html').click(function() {
-     $contextMenu.hide();
-});
-
-});
 
 
 function openCity(evt, button) {
@@ -33,34 +15,3 @@ function openCity(evt, button) {
         evt.currentTarget.className += " active";
     }
     
-    // Get the element with id="defaultOpen" and click on it
-    document.getElementById("defaultOpen").click();
-    // Get the modal
-    var modal = document.getElementById('myModal');
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-    
-    // When the user clicks the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    };
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    };
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
-    
-            let colName = document.getElementsByName("Collection name")[0].value;
-            let colDesc = document.getElementsByName("colDescription")[0].value;
-    
-            colName.innerText = "";
-            colDesc.innerHTML = "";
-        }
-    };
--- musiccollectorapi/src/main/webapp/JS/search1.js
+++ musiccollectorapi/src/main/webapp/JS/search1.js
@@ -1,5 +1,30 @@
 let del = 0;
 
+
+
+var selectedElment = null;
+$(function() {
+
+    var $contextMenu = $("#contextMenu");
+
+    $("body").on("contextmenu", "ul img", function(e) {
+        selectedElment = e.target.parentElement.parentElement;
+        console.log(selectedElment);
+        $contextMenu.css({
+            display: "block",
+            left: e.pageX,
+            top: e.pageY
+        });
+        return false;
+    });
+
+    $('html').click(function() {
+        $contextMenu.hide();
+    });
+
+});
+
+
 function searchapi() {
 
     let searchValue = document.getElementsByName("search")[0].value;
@@ -90,3 +115,58 @@ function searchapi() {
 
 }
 
+function addToCollection(){
+
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "http://localhost:8081/collections");
+    console.log("GET: "+xhr.response);
+    xhr.addEventListener("load", function loadCallback() {
+        switch (xhr.status) {
+            case 200:
+                console.log("Success GET" + JSON.parse(xhr.response));
+
+                break;
+            case 404:
+                console.log("Oups! Not found");
+                break;
+        }
+
+    });
+    xhr.send(null);
+}
+
+
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+
+        let colName = document.getElementsByName("Collection name")[0].value;
+        let colDesc = document.getElementsByName("colDescription")[0].value;
+
+        colName.innerText = "";
+        colDesc.innerHTML = "";
+    }
+};
