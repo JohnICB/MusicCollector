de1cb8b6e263127012bd58236317a2bcaf9b7b72
Unknown <guzicu.c@gmail.com>
2018-06-19 23:57:32 +0300

    add track

--- musiccollectorapi/src/main/webapp/JS/collection.js
+++ musiccollectorapi/src/main/webapp/JS/collection.js
@@ -102,3 +102,48 @@ function deleteElementFromCollection(liElement) {
     xhr.send(JSON.stringify(payload));
     
 }
+function openCity(evt, button) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(button).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+
+        let colName = document.getElementsByName("Collection name")[0].value;
+        let colDesc = document.getElementsByName("colDescription")[0].value;
+
+        colName.innerText = "";
+        colDesc.innerHTML = "";
+    }
+};
